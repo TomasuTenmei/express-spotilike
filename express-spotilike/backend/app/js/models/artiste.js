5f4b3e387e49b17fa34a4cc36cab2a92aa51a769
Tomasu <Thomas.ARNAUD.DEV@outlook.com>
2024-01-22 08:57:37 +0100

    modif

---
+++ express-spotilike/backend/app/js/models/artiste.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const artisteSchema = new mongoose.Schema({
+  nom_artiste: { type: String, required: true },
+  avatar: { type: String, required: true },
+  biographie: { type: Array, required: true }
+});
+
+module.exports = mongoose.model("Artiste", artisteSchema);
