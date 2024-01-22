f665c1f9489b55244334281f251cf452dac38ce2
Tomasu <Thomas.ARNAUD.DEV@outlook.com>
2024-01-22 14:07:19 +0100

    commit

--- backend/app/routes/artisteRoutes.js
+++ backend/app/routes/artisteRoutes.js
@@ -9,6 +9,8 @@ const {
     deleteArtiste
 } = require('../controllers/artisteController')
 
+// GET - /api/artists/:id/songs : Récupère la liste de tous les morceaux de l’artiste précisé par :id
+
 router.get('/', getArtistes)
 router.get('/:id', getArtiste)
 router.post('/', addArtiste)
