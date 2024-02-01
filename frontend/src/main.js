0503a73f0c2d4caba932bca857214294daebdabc
selma <salma.ayachi@insat.u-carthage.tn>
2024-02-01 23:14:35 +0100

    deleted some files

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import * as VueRouter from 'vue-router'
 import routes from "@/router";
-//import store from "@/store";
+
 
 
 const app = createApp(App);
 app.use(VueRouter.createRouter({history: VueRouter.createWebHistory(), routes}))
-//app.use(store);
 app.mount('#app');
